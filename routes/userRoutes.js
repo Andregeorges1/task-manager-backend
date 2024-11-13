503c0391a00f8b8015b0198512ac09a7d9855c57
Andregeorges1 <andre.georges03@gmail.com>
2024-11-13 14:11:58 +0200

    update register route to include authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { registerUser, loginUser, verifyOTP } = require('../controllers/userController');
+const authenticateToken = require('../middlewares/authMiddleware'); 
 const router = express.Router();
 
-router.post('/register', registerUser); 
+router.post('/register', authenticateToken,registerUser); 
 router.post('/login', loginUser);       
 router.post('/verify-otp', verifyOTP);  
 
